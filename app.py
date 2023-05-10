1f4b9a2ff1e863f9c650c4beb3a891b85627ae58
trd44 <trd44@cornell.edu>
2023-05-10 13:59:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,18 @@ app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 # CORS(app)
 api = Api(app)
 
-def load_openai_api_key():
-    try:
-        with open("API_KEY") as f:
-            file_contents = f.read()
-        os.environ["OPENAI_API_KEY"] = file_contents
-    except:
-        pass
+# def load_openai_api_key():
+#     try:
+#         with open("API_KEY") as f:
+#             file_contents = f.read()
+#         os.environ["OPENAI_API_KEY"] = file_contents
+#     except:
+#         pass
 
-# Load the API Key
-load_openai_api_key()
+# # Load the API Key
+# load_openai_api_key()
 
-saved_messages = [{"role": "system", "content": "respond to me as if you were a helpful travel agent helping me plan a trip."}]
+# saved_messages = [{"role": "system", "content": "respond to me as if you were a helpful travel agent helping me plan a trip."}]
 
 @app.route('/', defaults={'path':''})
 def serve(path):
@@ -33,53 +33,53 @@ def serve(path):
 
 api.add_resource(HelloApiHandler, '/flask/hello')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'build'), 'favicon.ico')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'build'), 'favicon.ico')
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def serve(path):
-#     if path != "" and os.path.exists(app.static_folder + '/' + path):
-#         return send_from_directory(app.static_folder, path)
-#     else:
-#         return send_from_directory(app.static_folder, 'index.html')
+# # @app.route('/', defaults={'path': ''})
+# # @app.route('/<path:path>')
+# # def serve(path):
+# #     if path != "" and os.path.exists(app.static_folder + '/' + path):
+# #         return send_from_directory(app.static_folder, path)
+# #     else:
+# #         return send_from_directory(app.static_folder, 'index.html')
 
-@app.route("/api/send_user_message", methods=['POST'])
-def send_user_message():
-    # Get the user's input
-    data = request.json
-    saved_messages.append({"role": "user", "content": data['newQuestion']})
+# @app.route("/api/send_user_message", methods=['POST'])
+# def send_user_message():
+#     # Get the user's input
+#     data = request.json
+#     saved_messages.append({"role": "user", "content": data['newQuestion']})
 
-    # Send the user's input to the ChatGPT API
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+#     # Send the user's input to the ChatGPT API
+#     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=saved_messages
-    )
+#     completion = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=saved_messages
+#     )
 
-    print(completion.choices[0].message)
+#     print(completion.choices[0].message)
 
-    saved_messages.append({"role": "assistant", "content": completion.choices[0].message.content})
-    # Return the ChatGPT's response
-    return completion.choices[0].message
+#     saved_messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+#     # Return the ChatGPT's response
+#     return completion.choices[0].message
 
-@app.route('/api/some_function', methods=['POST'])
-def some_function():
-    data = request.json
-    # Process the data and perform the desired function
-    result = {'result': 'Hello, ' + data['name']}
-    return jsonify(result)
+# @app.route('/api/some_function', methods=['POST'])
+# def some_function():
+#     data = request.json
+#     # Process the data and perform the desired function
+#     result = {'result': 'Hello, ' + data['name']}
+#     return jsonify(result)
 
-@app.route("/data")
-def get_time():
-    return{
-        'Name':"Tim",
-        'Age':"29",
-        'Date':x,
-        "Programming":"Python"
-    }
+# @app.route("/data")
+# def get_time():
+#     return{
+#         'Name':"Tim",
+#         'Age':"29",
+#         'Date':x,
+#         "Programming":"Python"
+#     }
 
 if __name__ == "__main__":
     # port = int(os.environ.get("PORT", 5000))
