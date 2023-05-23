4da4e0f8ccd3884849ffa71ef6c894b0387d3147
Christian <csw73@cornell.edu>
2023-05-23 16:31:19 -0700

    Setup of initial GPTModel service

---
+++ GPTModel.py
@@ -0,0 +1,40 @@
+import os
+import openai
+
+class GPTModel:
+
+    def __init__(self):
+        """
+        Initialize model usage
+        
+        Presumes that the API key is set in the environment already...
+        """
+        self.model_id = "gpt-3.5-turbo"
+        pass
+
+    def SetModel(self, model_id):
+        """
+        Set what model is used.
+        """
+        self.model_id = model_id
+
+    def MakeRequest(self, conversation_messages):
+        """
+        TODO: handle errors if wrong request
+        Make request to model
+
+        Args:
+            conversation_messages : messages input for api call
+
+        Return:
+            "assistant" message response if available
+        """
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        completion = openai.ChatCompletion.create(
+            model=self.model_id,
+            messages=conversation_messages
+        )
+        
+        return completion.choices[0].message
+
+
