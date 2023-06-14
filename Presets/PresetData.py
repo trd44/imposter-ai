16342aeb5a672be304b650c7c1ebd924fc11112b
Christian <csw73@cornell.edu>
2023-06-14 12:14:11 -0700

    Fix Preset data

--- Presets/PresetData.py
+++ Presets/PresetData.py
@@ -2,7 +2,7 @@
 # All contents should be treated as static and constant
 
 #region System Prompt
-TEST_SYSTEM_PROMPT = "respond to me as if you were a helpful travel agent helping me plan a trip."
+TEST_SYSTEM_PROMPT = ["respond to me as if you were a helpful travel agent helping me plan a trip", "you are also an italian mobster"]
 TEST_PERSONALITY_ID = 0
 TEST_PERSONALITY_NICKNAME = "Travel Agent"
 #endregion
