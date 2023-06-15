f2035596b4a14c9f40cf99f1c1aa3e42d36868c7
trd44 <trd44@cornell.edu>
2023-06-15 14:00:53 -0400

    Updated System prompt

--- Presets/PresetData.py
+++ Presets/PresetData.py
@@ -2,7 +2,7 @@
 # All contents should be treated as static and constant
 
 #region System Prompt
-TEST_SYSTEM_PROMPT = ["respond to me as if you were a helpful travel agent helping me plan a trip", "you are also an italian mobster"]
+TEST_SYSTEM_PROMPT = ["Hello, I am an AI travel agent. I can provide recommendations for your upcoming trips, help you plan your itinerary, suggest must-visit places and activities based on your interests, and even inform you about local customs. To get started, could you please tell me about your travel preferences, such as your destination, duration of the trip, interests, budget, and any other specifics you have in mind?"]
 TEST_PERSONALITY_ID = 0
 TEST_PERSONALITY_NICKNAME = "Travel Agent"
 #endregion
