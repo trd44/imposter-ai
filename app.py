170fae7ab9e05b6cad65d7cb094bb730692154e8
trd44 <trd44@cornell.edu>
2023-06-14 22:15:14 -0400

    Cleaned up App.js and app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 #region General/API Imports
 import os
 import datetime
-import openai
-from flask import Flask, request, jsonify, send_from_directory, render_template, g
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, request, send_from_directory, g
+from flask_restful import Api
 from flask_cors import CORS
+import openai
 #endregion
 
 #region Backend Imports
@@ -21,11 +21,9 @@ from backend.DatabaseManager import DatabaseManager
 from Presets.PresetData import TEST_PERSONALITY_ID 
 #endregion
 
-#region Global Variables
-x = datetime.datetime.now()
-#endregion
-
 #region Application Start
+
+# Initialize Flask app and CORS, set up API and secret key
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 CORS(app)
 api = Api(app)
@@ -34,46 +32,36 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'imposter.sqlite'),
 )
 
-# ensure the instance folder exists
+# Ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
 except OSError:
     pass
 
 # Load the API Key
-# TODO: How to handle API Key?
 cb.load_openai_api_key()
+
 #endregion
 
-
-# TODO: Clean this up...
+# Serve the static files in the build directory
 @app.route('/', defaults={'path': ''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
-
+# Handle favicon requests
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'build'), 'favicon.ico')
 
+# Initialize the database and register the auth blueprint
 from backend import db
 db.init_app(app)
-
 from backend import auth
 app.register_blueprint(auth.bp)
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def serve(path):
-#     if path != "" and os.path.exists(app.static_folder + '/' + path):
-#         return send_from_directory(app.static_folder, path)
-#     else:
-#         return send_from_directory(app.static_folder, 'index.html')
-
-saved_messages = [{"role": "system", "content": "respond to me as if you were a helpful travel agent helping me plan a trip."}]
-
 #region ChatMessaging
+
+# API endpoint to handle sending user messages
 @app.route("/api/send_user_message", methods=['POST'])
 @login_required
 def send_user_message():
@@ -86,63 +74,29 @@ def send_user_message():
     # Send message
     # TODO: update for being able to select personality
     response = chat_manager.SendMessage(TEST_PERSONALITY_ID, data['newMessage'])
-    print("Sent Message")
 
-    print(response.content)
-
-    # Return the ChatGPT's response
+    # Return ChatGPT's response
     return response
 
+# API endpoint to fetch chat history
 @app.route("/api/fetch_chat_history", methods=['GET'])
-# @login_required
+@login_required
 def fetch_chat_history():
-    print("/api/fetch_chat_history called")
-    # Get the user's input
-    # data = request.json
-
     # Startup chat manager
     chat_manager = ChatManager(g.user['id'], None, GPTModel())
     conversation = chat_manager.RetrieveConversation(TEST_PERSONALITY_ID)
 
-    response = conversation.ExportSavedMessages()
+    message_history = conversation.ExportSavedMessages()
 
-    print(response)
-
-    # Return the ChatGPT's response
-    return response
-
-# #TODO: Add login as required...
-# @app.route("/api/old/send_user_message", methods=['POST'])
-# def old_send_user_message():
-#     # Get the user's input
-#     data = request.json
-
-#     saved_messages.append({"role": "user", "content": data['newMessage']})
-#     # Send the user's input to the ChatGPT API
-#     openai.api_key = os.getenv("OPENAI_API_KEY")
-#     completion = openai.ChatCompletion.create(
-#     model="gpt-3.5-turbo",
-#     messages=saved_messages
-#     )
-#     print(completion.choices[0].message)
-#     saved_messages.append({"role": "assistant", "content": completion.choices[0].message.content})
-#     # Return the ChatGPT's response
-#     return completion.choices[0].message
-
-# @app.route('/api/some_function', methods=['POST'])
-# def some_function():
-#     data = request.json
-#     # Process the data and perform the desired function
-#     result = {'result': 'Hello, ' + data['name']}
-#     return jsonify(result)
+    return message_history
 
 #endregion
 
+# Handle 404 errors by sending the index.html static file
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
 
+# Main entry point
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 // src/App.js
 
+// Import necessary packages and components
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Chat from './components/Chat/Chat'
 import Footer from './components/Footer/Footer';
@@ -12,38 +13,50 @@ import Register from './components/Register/Register';
 
 import "./App.css";
 
+// Main App component
 const App = () => {
+  // State for storing user token
   const [token, setToken] = useState();
+
+  // State for storing username, initialised with username from local storage if it exists
   const [username, setUsername] = useState(localStorage.getItem('username') || '');
 
   // This effect runs once when the component mounts
   useEffect(() => {
+    // Check if a user token is stored in local storage
     const storedToken = localStorage.getItem('token');
+
+    // If a stored token exists, set it as the current token state
     if (storedToken) {
       setToken(storedToken);
     }
-  }, []); // The empty array means this effect runs once on mount and not on updates  
+  }, []); // This effect runs once on mount and not on updates  
 
   return (
     <div className="App">
       <Router>
-      <NavBar token={token} setToken={setToken} username={username} setUsername={setUsername} />
+        {/* Pass token and username states to NavBar */}
+        <NavBar token={token} setToken={setToken} username={username} setUsername={setUsername} />
 
+        {/* If user is not authenticated, only show login and register routes */}
+        {/* If user is authenticated, show chat route in addition to home */}
         {!token ? (
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login setToken={setToken} setUsername={setUsername} />} />
             <Route path="/register" element={<Register setToken={setToken} setUsername={setUsername} />} />
-            {/* <Route path="/chat" element={<Chat />} /> */}
+            {/* If any other path is visited, redirect to home */}
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         ) : (
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/chat" element={<Chat />} />
+            {/* If any other path is visited, redirect to home */}
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         )}
+        {/* Display Footer on all pages */}
         <Footer />
       </Router>
     </div>
