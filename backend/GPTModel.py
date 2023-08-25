95a37e70c3156aac8143a473610856de98a58b22
Christian <csw73@cornell.edu>
2023-08-25 09:16:35 -0700

    Updated error handling for each error type

--- backend/GPTModel.py
+++ backend/GPTModel.py
@@ -33,8 +33,6 @@ class GPTModel:
         # TODO: Might need to change how key is set. Should only be set once?
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
-        # [CHECK HERE FOR ERRORS THAT WOULD CRASH PROGRAM]
-        # HANDLE ALL API ERRORS 
         ret = None
         try:
             completion = openai.ChatCompletion.create(
@@ -44,9 +42,45 @@ class GPTModel:
             print("Model Completion:")
             print(completion)
             ret = completion.choices[0].message
-        # except openai.error.OpenAIApiError as e:
-        #     # Handle all errors
-        #     print(f"OpenAI API returned an API Error: {e}")
+        ### API ERROR HANDLING
+        except openai.error.APIError as e:
+            #Handle API error here, e.g. retry or log
+            print(f"OpenAI API returned an API Error: {e}")
+        
+        ## [SERVICE ERRORS]
+        except openai.error.APIConnectionError as e:
+            #Handle connection error here
+            print(f"Failed to connect to OpenAI API: {e}")
+        except openai.error.ServiceUnavailableError as e:
+            #Handle service unavailabe error
+            print(f"OpenAI API service is currently unavailable: {e}")
+        except openai.error.RateLimitError as e:
+            #Handle rate limit error (we recommend using exponential backoff)
+            print(f"OpenAI API request exceeded rate limit: {e}")
+        except (openai.error.Timeout, openai.error.TryAgain) as e:
+            #Handle timeout error (retry after a few seconds)
+            print(f"OpenAI API request timeout: {e}")
+        
+        ## [AUTHENTIFICATION ERRORS]
+        except openai.error.AuthenticationError as e:
+            #Handle authetication error
+            print(f"OpenAI API key cannot be authenticated: {e}")
+        except openai.error.PermissionError as e:
+            #Handle permission error
+            print(f"OpenAI API request does not have permission: {e}")    
+        except openai.error.SignatureVerificationError as e:
+            #Handle signature verification error
+            print(f"OpenAI API signature cannot be verified: {e}")
+
+        ## [REQUEST ERRORS]
+        except openai.error.InvalidAPIType as e:
+            #Handle invalid API Type error
+            print(f"OpenAI API request contained invalid API Type: {e}")
+        except openai.error.InvalidRequestError as e:
+            #Handle invalid request error
+            print(f"OpenAI API request is invalid: {e}")   
+
+        ## [UNKNOWN ERROR]
         except Exception as e:
             print(f"An unknown error occured: {e}")
         
