5aa7d2b48341d46131ed380b80ef06401d38c8fd
Christian <csw73@cornell.edu>
2023-09-07 10:57:25 -0700

    Outline frontend changes for multiple personalities

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -16,6 +16,9 @@ export default function Chat() {
   const [name, setName] = useState('');
   const [newMessage, setNewMessage] = useState('');
   const [isTyping, setIsTyping] = useState(false);
+
+  //TODO: mMake a function like fetchChatHistory where it requests all the personalities from the database and adds them
+  //currently when we access contacts, we just index at zero it seems.
   const [contacts, setContacts] = useState([
     { id: 1, name: 'Travel Agent', image: myImage, lastMessage: 'Hey there!' }, // Test Data
     // { id: 2, name: 'Jane Smith', image: myImage, lastMessage: 'See you tomorrow' },
@@ -26,6 +29,8 @@ export default function Chat() {
 
   useEffect(() => {
     // Fetch contacts from the server and setContacts
+    // TODO: provide which personality id to get conversation for. Assume that conversation exists in backend even if not talked to before.
+    // Backend handles new covnersations
     const fetchChatHistory = async () => {
       console.log("trying to call api/fetch_chat_history");
       try {
@@ -48,6 +53,13 @@ export default function Chat() {
     fetchChatHistory();
   }, []);
 
+  //TODO: This is where the user clicks the contact. does not swap anything. Would have to update this function
+  //Would have to fetch chat history and provide the correct chat ID
+  // [1] update the message history that is displayed (would recall 'fetchChatHistory)
+  // [2] update some state so when new message is sent, we get correct response
+  // [3] should block changing contacts until we get response from last message? (another ticket)
+  // dont want wrong personality to have response displayed
+  // maybe when get response, and not on correct ID, dont display it. 
   const handleContactClick = (contactId) => {
     // TODO: Handle when a contact is clicked
     console.log(`Contact clicked: ${contactId}`);
