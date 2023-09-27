7f70b946157287651579c031d2b7d6c9d89dfdf9
Christian <csw73@cornell.edu>
2023-09-27 09:48:16 -0700

    Add preset data for contact image

--- Presets/PresetData.py
+++ Presets/PresetData.py
@@ -5,4 +5,5 @@
 TEST_SYSTEM_PROMPT = ["Hello, I am an AI travel agent. I can provide recommendations for your upcoming trips, help you plan your itinerary, suggest must-visit places and activities based on your interests, and even inform you about local customs. To get started, could you please tell me about your travel preferences, such as your destination, duration of the trip, interests, budget, and any other specifics you have in mind?"]
 TEST_PERSONALITY_ID = 0
 TEST_PERSONALITY_NICKNAME = "Travel Agent"
+TEST_PERSONALITY_IMG = "TestContact.jpeg"
 #endregion
