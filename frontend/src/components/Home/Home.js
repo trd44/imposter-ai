a3fdb16dc78d9ba196a3a3d56a0b87560c0714d1
trd44 <trd44@cornell.edu>
2023-11-15 12:39:28 -0500

    Reformatted Home.js

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,18 +1,28 @@
 // src/components/Home/Home.js
 
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 import './Home.css';
 
-const Home = ({ token }) => {
+/**
+ * Home page component
+ * @param {string} token - JWT token
+ * @return {JSX.Element}
+ */
+const Home = ({token}) => {
+  // useNavigate hook from react-router-dom
   const navigate = useNavigate();
 
+  /**
+   * Handle start chatting button click
+   * @return {void}
+   */
   const handleStartChatting = () => {
     if (token) { // if token is present, user is logged in
-      navigate("/chat");
+      navigate('/chat');
     } else { // if token is not present, user is not logged in
-      navigate("/register");
+      navigate('/register');
     }
   };
 
@@ -20,38 +30,66 @@ const Home = ({ token }) => {
     <div className='home-container'>
       <header>
         <h1>Welcome to imposter.ai 🤖</h1>
-        <p>Start conversations with AI in unique roles – from travel agents to historical figures.</p>
+        <p>Start conversations with unique and interesting AI personalities.</p>
         <button onClick={handleStartChatting}>Start Chatting</button>
       </header>
 
       <section>
         <h2>How It Works</h2>
-        <p>Imposter.AI uses the power of ChatGPT combined with our innovative system prompts to make our AI behave in specific ways...</p>
+        <p>Imposter.AI uses the power of ChatGPT combined with our innovative
+          system prompts to make our AI behave in specific ways...</p>
       </section>
 
-      <br /> {/* This is to make the sections look separate but there is probably a better way to do this with CSS. */}
+      <br />
+
+      <section>
+        <h2>Our Personalities</h2>
+        <p>David Attenborough</p>
+        <p>Anthony Bourdain</p>
+        <p>Bill Gates</p>
+        <p>Kobe Bryant</p>
+        <p>Lil Wayne</p>
+        <p>Henry McCord</p>
+        <p>Thor</p>
+        <p>Severus Snape</p>
+        <p>Gollum</p>
+        <p>Tyrion Lannister</p>
+        <p>More coming soon...</p>
+      </section>
 
       <section>
         <h2>Updates</h2>
-        <p>We have our first personality for ChatGPT to impersonate, a travel agent, up and running. Please try it out!
+        <p>Our initial personalities are up and running!
           <br />
           <br />
-          New personalities will be coming in the near future.
+          We are working on adding a create custom personality feature.
         </p>
       </section>
 
-      <br /> {/* This is to make the sections look separate but there is probably a better way to do this with CSS. */}
+      <br />
 
       <section>
         <h2>About Us</h2>
-        <p>We're two friends, Tim and Christian, passionate about AI and its potential...
+        <p>We`&apos;`re two friends, Tim and Christian,
+          passionate about AI and its potential...
         <br />
         <br />
-        If you have any suggestions, questions, or want to reach out in general please email us at <a href="mailto:imposter.ai.2023@gmail.com" target='_blank' rel='noopener noreferrer'>imposter.ai.2023@gmail.com</a>
+        If you have any suggestions, questions,
+        or want to reach out in general please email us at
+        <a
+          href="mailto:imposter.ai.2023@gmail.com"
+          target='_blank'
+          rel='noopener noreferrer'>
+            imposter.ai.2023@gmail.com
+        </a>
         </p>
       </section>
     </div>
   );
+};
+
+Home.propTypes = {
+  token: PropTypes.string,
 };
 
 export default Home;
