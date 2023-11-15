54581514c1d52b017b8bc64b651630d6c9050aed
trd44 <trd44@cornell.edu>
2023-11-14 23:29:16 -0500

    Added ESLintr. Refactored App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,37 @@
 // src/App.js
 
 // Import necessary packages and components
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
-import Chat from './components/Chat/Chat'
+import Chat from './components/Chat/Chat';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
 import Register from './components/Register/Register';
 
-import "./App.css";
+import './App.css';
 
 // Main App component
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  // State for storing user token
   const [token, setToken] = useState();
-
-  // State for storing username, initialised with username from local storage if it exists
-  const [username, setUsername] = useState(localStorage.getItem('username') || '');
+  const initialUsername = localStorage.getItem('username') || '';
+  const [username, setUsername] = useState(initialUsername);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    document.title = "imposter.ai";
-  }, []);
+    document.title = 'imposter.ai';
 
-  // This effect runs once when the component mounts
-  useEffect(() => {
-    // Check if a user token is stored in local storage
     const storedToken = localStorage.getItem('token');
-    const storedTokenExpiry = Number(localStorage.getItem('tokenExpiry')) * 1000; // Convert to milliseconds
+    const tokenExpiryValue = localStorage.getItem('tokenExpiry');
+    const storedTokenExpiry = Number(tokenExpiryValue) * 1000;
 
-    // If the stored token exists and it hasn't expired, set it as the current token state
     if (storedToken && new Date().getTime() < storedTokenExpiry) {
       setToken(storedToken);
     } else {
@@ -42,11 +41,11 @@ const App = () => {
       localStorage.removeItem('username');
     }
 
-    setLoading(false); // Set loading to false after checking for token
-  }, []); // This effect runs once on mount and not on updates  
+    setLoading(false);
+  }, []);
 
   if (loading) {
-    return <div>Loading...</div>; // Replace with your actual loading component or spinner
+    return <div>Loading...</div>;
   }
 
   const handleSuccessfulLogin = (token, tokenExpiry, username) => {
@@ -55,37 +54,45 @@ const App = () => {
     localStorage.setItem('tokenExpiry', tokenExpiry);
     setUsername(username);
     localStorage.setItem('username', username);
-  }
+  };
 
   return (
     <div className="App">
       <Router>
-        {/* Pass token and username states to NavBar */}
-        <NavBar token={token} setToken={setToken} username={username} setUsername={setUsername} />
+        <NavBar
+          token={token} setToken={setToken}
+          username={username}
+          setUsername={setUsername}
+        />
 
-        {/* If user is not authenticated, only show login and register routes */}
-        {/* If user is authenticated, show chat route in addition to home */}
         {!token ? (
           <Routes>
             <Route path="/" element={<Home token={token} />} />
-            <Route path="/login" element={<Login onSuccessfulLogin={handleSuccessfulLogin} />} />
-            <Route path="/register" element={<Register setToken={setToken} setUsername={setUsername} />} />
-            {/* If any other path is visited, redirect to home */}
+            <Route
+              path="/login"
+              element={<Login onSuccessfulLogin={handleSuccessfulLogin} />}
+            />
+            <Route
+              path="/register"
+              element={
+                <Register
+                  setToken={setToken}
+                  setUsername={setUsername}
+                />}
+            />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         ) : (
           <Routes>
             <Route path="/" element={<Home token={token} />} />
             <Route path="/chat" element={<Chat />} />
-            {/* If any other path is visited, redirect to home */}
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         )}
-        {/* Display Footer on all pages */}
         <Footer />
       </Router>
     </div>
-  )
+  );
 };
 
 export default App;
