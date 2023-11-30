62b6539b35d7f0b1c7fcbdc49dab3ccc87379f56
Christian <csw73@cornell.edu>
2023-11-30 11:07:08 -0800

    cleanup functions and comments

--- ManagePersonalityDatabase.py
+++ ManagePersonalityDatabase.py
@@ -4,54 +4,70 @@ import json
 import sqlite3
 from backend.Utils import SerializeJson
 
-def CreateNewPersonalityFromJSON(json_filename):
+def CreateNewPersonalityFromJSON(json_filename: str):
     """
     Will create a new personality in the personality table from a JSON file.
+    
+    args:
+        json_filename: JSON filename (absolute path)
     """
+    # [1] Extract personality data from JSON file
     with open(json_filename, 'r') as file:
         data = json.load(file)
 
-    # Connect to the SQLite database
+    # [2] Connect to the SQLite database
     conn = sqlite3.connect("instance/imposter.sqlite")
     cursor = conn.cursor()
 
-    # Insert data from the JSON file into the database
+    # [3] Insert data from the JSON file into the database
     cursor.execute("""
         INSERT INTO personalities (NAME, SYSTEM_PROMPT, INTRO_MESSAGE, IMAGE_PATH)
         VALUES (?, ?, ?, ?)
     """, (data['name'], SerializeJson(data['system_prompt']), data['intro_message'], data['image_path']))
 
-    # Commit changes and close the connection
+    # [4] Commit changes and close the connection
     conn.commit()
     conn.close()
 
     print("Data inserted successfully!")
 
-def delete_personality(personality_id):
-    # Connect to the SQLite database
+def delete_personality(personality_id: int):
+    """
+    Will delete personality from personality table.
+
+    args:
+        personality_id: ID of personality to remove from table
+    """
+    # [1] Connect to the SQLite database
     conn = sqlite3.connect("instance/imposter.sqlite")
     cursor = conn.cursor()
 
-    # Delete record based on ID
+    # [2] Delete record based on ID
     cursor.execute("DELETE FROM personalities WHERE ID = ?", (personality_id,))
 
-    # Commit changes and close the connection
+    # [3] Commit changes and close the connection
     conn.commit()
     conn.close()
 
     print(f"Record with ID {personality_id} deleted successfully!")
 
-def update_personality(personality_id, json_filename):
-    """Update the personality by its ID."""
+def update_personality(personality_id: int, json_filename: str):
+    """
+    Update the personality by its ID from JSON file.
 
+    args:
+        personality_id: ID of personality to update
+        json_filename: JSON filename (absolute path)
+    """
+    # [1] Extract personality data from JSON file
     with open(json_filename, 'r') as file:
         data = json.load(file)
     
-    # Connect to the SQLite database
+    # [2] Connect to the SQLite database
     conn = sqlite3.connect("instance/imposter.sqlite")
     cursor = conn.cursor()
 
-    # Update the nickname for the specified personality ID
+    # [3] Update the nickname for the specified personality ID
     cursor.execute("""
         UPDATE personalities
         SET 
@@ -61,40 +77,39 @@ def update_personality(personality_id, json_filename):
             IMAGE_PATH = ?
         WHERE ID = ?
     """, (data['name'], SerializeJson(data['system_prompt']), data['intro_message'], data['image_path'], personality_id))
-    # Commit changes and close the connection
+    # [4] Commit changes and close the connection
     conn.commit()
     conn.close()
 
     print(f"Personality {personality_id} updated successfully!")
-    pass
 
 def main():
-    # Set up the argument parser
+    """
+    Take appropriate actions to update databse given the aruments provided by the user.
+    """
+    # [1] Set up the argument parser
     parser = argparse.ArgumentParser(description='Manage data in the personalities SQLite database.')
 
-    # Subparsers for different operations
+    # [2] Subparsers for different operations
     subparsers = parser.add_subparsers(dest='operation', required=True)
 
-    # Parser for inserting data
+    # [3] Parser for inserting data
     insert_parser = subparsers.add_parser('insert', help='Insert data from a JSON file into the database. Args [json_file_path]]')
     insert_parser.add_argument('json_file', help='Path to the JSON file.')
 
-    # Parser for deleting a record
+    # [4] Parser for deleting a record
     delete_parser = subparsers.add_parser('delete', help='Delete a record from the database using its ID. Args [personality_id]')
     delete_parser.add_argument('personality_id', type=int, help='ID of the record to delete.')
 
-    # Parser for updating a record
+    # [5] Parser for updating a record
     update_parser = subparsers.add_parser('update', help='Update a record from the database using its ID. Args [personality_id, json_file_path]')
     update_parser.add_argument('personality_id', type=int, help='ID of the record to update.')
     update_parser.add_argument('json_file', help='Path to the JSON file.')
 
-    # Parse the arguments
-    args = parser.parse_args()
+    # [6] Parse the arguments
+    args = parser.parse_args()    
 
-    # Call the function
-    
-
-    # Call the appropriate function based on the operation
+    # [7] Call the appropriate function based on the operation
     if args.operation == 'insert':
         CreateNewPersonalityFromJSON(args.json_file)
     elif args.operation == 'delete':
