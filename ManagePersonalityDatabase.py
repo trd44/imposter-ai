011af098336d7ad5b9b223d12ee0328169b39293
Christian <csw73@cornell.edu>
2023-12-03 13:56:30 -0800

    update database scripting tool to meet Black std + add file header

--- ManagePersonalityDatabase.py
+++ ManagePersonalityDatabase.py
@@ -1,18 +1,37 @@
+"""
+ManagePersonalityDatabas.py
 
+Author: Tim Duggan
+Date: 12/3/2023
+Company: ImposterAI
+Contact: csw73@cornell.edu
+
+This is a Python scripting tool for updating and modifying the personality table
+in the ImposerAI database.
+"""
+# region General/API Imports
 import argparse
 import json
 import sqlite3
+
+# endregion
+
+# region Backend Imports
 from backend.Utils import SerializeJson
 
+# endregion
+
+
+# region Table Manipulation Functions
 def CreateNewPersonalityFromJSON(json_filename: str):
     """
     Will create a new personality in the personality table from a JSON file.
-    
+
     Args:
         json_filename (str): JSON filename (absolute path)
     """
     # [1] Extract personality data from JSON file
-    with open(json_filename, 'r') as file:
+    with open(json_filename, "r") as file:
         data = json.load(file)
 
     # [2] Connect to the SQLite database
@@ -20,16 +39,25 @@ def CreateNewPersonalityFromJSON(json_filename: str):
     cursor = conn.cursor()
 
     # [3] Insert data from the JSON file into the database
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT INTO personalities (NAME, SYSTEM_PROMPT, INTRO_MESSAGE, IMAGE_PATH)
         VALUES (?, ?, ?, ?)
-    """, (data['name'], SerializeJson(data['system_prompt']), data['intro_message'], data['image_path']))
+    """,
+        (
+            data["name"],
+            SerializeJson(data["system_prompt"]),
+            data["intro_message"],
+            data["image_path"],
+        ),
+    )
 
     # [4] Commit changes and close the connection
     conn.commit()
     conn.close()
 
     print("Data inserted successfully!")
+
 
 def delete_personality(personality_id: int):
     """
@@ -51,6 +79,7 @@ def delete_personality(personality_id: int):
 
     print(f"Record with ID {personality_id} deleted successfully!")
 
+
 def update_personality(personality_id: int, json_filename: str):
     """
     Update the personality by its ID from JSON file.
@@ -60,15 +89,16 @@ def update_personality(personality_id: int, json_filename: str):
         json_filename (str): JSON filename (absolute path)
     """
     # [1] Extract personality data from JSON file
-    with open(json_filename, 'r') as file:
+    with open(json_filename, "r") as file:
         data = json.load(file)
-    
+
     # [2] Connect to the SQLite database
     conn = sqlite3.connect("instance/imposter.sqlite")
     cursor = conn.cursor()
 
     # [3] Update the nickname for the specified personality ID
-    cursor.execute("""
+    cursor.execute(
+        """
         UPDATE personalities
         SET 
             NAME = ?,
@@ -76,47 +106,75 @@ def update_personality(personality_id: int, json_filename: str):
             INTRO_MESSAGE = ?,
             IMAGE_PATH = ?
         WHERE ID = ?
-    """, (data['name'], SerializeJson(data['system_prompt']), data['intro_message'], data['image_path'], personality_id))
+    """,
+        (
+            data["name"],
+            SerializeJson(data["system_prompt"]),
+            data["intro_message"],
+            data["image_path"],
+            personality_id,
+        ),
+    )
     # [4] Commit changes and close the connection
     conn.commit()
     conn.close()
 
     print(f"Personality {personality_id} updated successfully!")
 
+
+# endregion
+
+
 def main():
     """
     Take appropriate actions to update databse given the aruments provided by the user.
     """
     # [1] Set up the argument parser
-    parser = argparse.ArgumentParser(description='Manage data in the personalities SQLite database.')
+    parser = argparse.ArgumentParser(
+        description="Manage data in the personalities SQLite database."
+    )
 
     # [2] Subparsers for different operations
-    subparsers = parser.add_subparsers(dest='operation', required=True)
+    subparsers = parser.add_subparsers(dest="operation", required=True)
 
     # [3] Parser for inserting data
-    insert_parser = subparsers.add_parser('insert', help='Insert data from a JSON file into the database. Args [json_file_path]]')
-    insert_parser.add_argument('json_file', help='Path to the JSON file.')
+    insert_parser = subparsers.add_parser(
+        "insert",
+        help="Insert data from a JSON file into the database. Args [json_file_path]]",
+    )
+    insert_parser.add_argument("json_file", help="Path to the JSON file.")
 
     # [4] Parser for deleting a record
-    delete_parser = subparsers.add_parser('delete', help='Delete a record from the database using its ID. Args [personality_id]')
-    delete_parser.add_argument('personality_id', type=int, help='ID of the record to delete.')
+    delete_parser = subparsers.add_parser(
+        "delete",
+        help="Delete a record from the database using its ID. Args [personality_id]",
+    )
+    delete_parser.add_argument(
+        "personality_id", type=int, help="ID of the record to delete."
+    )
 
     # [5] Parser for updating a record
-    update_parser = subparsers.add_parser('update', help='Update a record from the database using its ID. Args [personality_id, json_file_path]')
-    update_parser.add_argument('personality_id', type=int, help='ID of the record to update.')
-    update_parser.add_argument('json_file', help='Path to the JSON file.')
+    update_parser = subparsers.add_parser(
+        "update",
+        help="Update a record from the database using its ID. "
+        + "Args [personality_id, json_file_path]",
+    )
+    update_parser.add_argument(
+        "personality_id", type=int, help="ID of the record to update."
+    )
+    update_parser.add_argument("json_file", help="Path to the JSON file.")
 
     # [6] Parse the arguments
-    args = parser.parse_args()    
+    args = parser.parse_args()
 
     # [7] Call the appropriate function based on the operation
-    if args.operation == 'insert':
+    if args.operation == "insert":
         CreateNewPersonalityFromJSON(args.json_file)
-    elif args.operation == 'delete':
+    elif args.operation == "delete":
         delete_personality(args.personality_id)
-    elif args.operation == 'update':
+    elif args.operation == "update":
         update_personality(args.personality_id, args.json_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
