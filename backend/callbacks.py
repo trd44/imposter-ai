85c4595c4fe0e6ca0b9fbcfae4117e6fe7300ca4
Christian <csw73@cornell.edu>
2023-12-03 16:04:40 -0800

    cleanup callbacks.py

--- backend/callbacks.py
+++ backend/callbacks.py
@@ -1,9 +1,24 @@
+"""
+callbacks.py
+
+Author: Tim Duggan
+Date: 12/3/2023
+Company: ImposterAI
+Contact: csw73@cornell.edu
+
+Contains callback functions.
+"""
+
+# region General/API imports
 import os
+
+# endregion
+
 
 def load_openai_api_key():
     try:
         with open("API_KEY") as f:
             file_contents = f.read()
         os.environ["OPENAI_API_KEY"] = file_contents
-    except:
-        pass
+    except Exception as e:
+        print(f"error: cannot read api key! {str(e)}")
