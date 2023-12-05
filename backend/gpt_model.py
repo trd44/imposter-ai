5dcec489911062d701895f488590ede811288af0
Christian <csw73@cornell.edu>
2023-12-04 23:42:10 -0800

    cleanup gpt model

--- backend/gpt_model.py
+++ backend/gpt_model.py
@@ -1,92 +1,108 @@
+"""
+gpt_model.py
+
+Author: Christian Welling
+Date: 12/3/2023
+Company: ImposterAI
+Contact: csw73@cornell.edu
+
+Contains class for accessing GPTModels through the openai API. Must specify
+what model id is to be used as well as additional parameters needed to
+make a successful OpenAI API request.
+"""
+
+# region General/API Imports
 import os
 import openai
 
-class GPTModel:
+# endregion
 
+# region Backend Imports
+
+from backend.ai_model import AIModel
+
+# endregion
+
+
+class GPTModel(AIModel):
     def __init__(self):
         """
-        Initialize model usage
-        
+        Access OpenAI GPT model with class.
+
         Presumes that the API key is set in the environment already...
         """
-        #self.model_id = "gpt-3.5-turbo"
-        self.model_id = "gpt-4"
-        pass
+        self._model_id = "gpt-4"
 
-    def SetModel(self, model_id):
+    def set_model(self, model_id: str) -> None:
         """
-        Set what model is used.
+        Sets the id of the model to be used.
+
+        Arguments:
+            model_id (str): The id of the model to be set.
         """
-        self.model_id = model_id
-
-    def MakeRequest(self, conversation_messages):
-        """
-        TODO: handle errors if wrong request
-        Make request to model
-
-        Args:
-            conversation_messages : messages input for api call
-
-        Return:
-            "assistant" message response if available
-            None if error
-        """
-        # TODO: Might need to change how key is set. Should only be set once?
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-
-        ret = None
-        try:
-            completion = openai.ChatCompletion.create(
-                model=self.model_id,
-                messages=conversation_messages,
-                temperature=1.2,
-            )
-            print("Model Completion:")
-            print(completion)
-            ret = completion.choices[0].message
-        ### API ERROR HANDLING
-        ## [SERVICE ERRORS]
-        except openai.error.APIConnectionError as e:
-            #Handle connection error here
-            print(f"Failed to connect to OpenAI API: {e}")
-        except openai.error.ServiceUnavailableError as e:
-            #Handle service unavailabe error
-            print(f"OpenAI API service is currently unavailable: {e}")
-        except openai.error.RateLimitError as e:
-            #Handle rate limit error (we recommend using exponential backoff)
-            print(f"OpenAI API request exceeded rate limit: {e}")
-        except (openai.error.Timeout, openai.error.TryAgain) as e:
-            #Handle timeout error (retry after a few seconds)
-            print(f"OpenAI API request timeout: {e}")
-        
-        ## [AUTHENTIFICATION ERRORS]
-        except openai.error.AuthenticationError as e:
-            #Handle authetication error
-            print(f"OpenAI API key cannot be authenticated: {e}")
-        except openai.error.PermissionError as e:
-            #Handle permission error
-            print(f"OpenAI API request does not have permission: {e}")    
-        except openai.error.SignatureVerificationError as e:
-            #Handle signature verification error
-            print(f"OpenAI API signature cannot be verified: {e}")
-
-        ## [REQUEST ERRORS]
-        except openai.error.InvalidAPIType as e:
-            #Handle invalid API Type error
-            print(f"OpenAI API request contained invalid API Type: {e}")
-        except openai.error.InvalidRequestError as e:
-            #Handle invalid request error
-            print(f"OpenAI API request is invalid: {e}")   
-
-        ## [GENERAL API ERROR]
-        except openai.error.APIError as e:
-            #Handle API error here, e.g. retry or log
-            print(f"OpenAI API returned an API Error: {e}")
-
-        ## [UNKNOWN ERROR]
-        except Exception as e:
-            print(f"An unknown error occured: {e}")
-        
-        return ret
+        # Set the model id
+        self._model_id = model_id
 
 
+def make_request(self, conversation_messages):
+    """
+    Makes a request to the model.
+
+    Args:
+        conversation_messages (json): Input messages for the api call.
+
+    Returns:
+        JSON: The message response received from the "assistant".
+              Returns None if there's an error.
+    """
+
+    # Assigns the environment variable for the OPENAI API KEY to openai.api_key.
+    # TODO: This should ideally be done once, perhaps during initialization.
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    ret = None
+    # Make API request
+    try:
+        completion = openai.ChatCompletion.create(
+            model=self._model_id,
+            messages=conversation_messages,
+            temperature=1.2,
+        )
+        ret = completion.choices[0].message
+
+    # Error handling for different types of API errors, correctly categorized
+    # Section 1: Service Errors
+    except openai.error.APIConnectionError as e:
+        print(f"Failed to connect to OpenAI API: {e}")
+    except openai.error.ServiceUnavailableError as e:
+        print(f"OpenAI API service is currently unavailable: {e}")
+    except openai.error.RateLimitError as e:
+        print(f"OpenAI API request exceeded rate limit: {e}")
+    except (openai.error.Timeout, openai.error.TryAgain) as e:
+        print(f"OpenAI API request timeout: {e}")
+
+    # Section 2: Authentication Errors
+    except openai.error.AuthenticationError as e:
+        print(f"OpenAI API key cannot be authenticated: {e}")
+    except openai.error.PermissionError as e:
+        print(f"OpenAI API request does not have permission: {e}")
+    except openai.error.SignatureVerificationError as e:
+        print(f"OpenAI API signature cannot be verified: {e}")
+
+    # Section 3: Request Errors
+    except openai.error.InvalidAPIType as e:
+        print(f"OpenAI API request contained invalid API Type: {e}")
+    except openai.error.InvalidRequestError as e:
+        print(f"OpenAI API request is invalid: {e}")
+
+    # Section 4: General Errors
+    except openai.error.APIError as e:
+        # Handle general API error here, for instance, retry or log the error.
+        print(f"OpenAI API returned an API Error: {e}")
+
+    # Section 5: Unknown Errors
+    except Exception as e:
+        # Handle any other unknown errors
+        print(f"An unknown error occurred: {e}")
+
+    return ret
